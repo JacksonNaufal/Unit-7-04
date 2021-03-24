2a7b04673ddac5cca265b351bc4b9367fcb148eb
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-24 18:15:26 +0000

    spaces

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let age = 0
 let weekend = 0
+// These define the variables and what they mean
 document.getElementById('button').addEventListener('click', button)
-
+// These allow the button to function and click
 function button () {
   age = document.getElementById('input').value
   weekend = document.getElementById('input2').value
   if ((age < 14 && weekend === 'Sunday') || (age < 14 && weekend === 'Saturday')) {
     document.getElementById('para').innerHTML = (' You may go to the Museum for Free!')
-  } else if ((age >= 14 && weekend === 'Sunday') || (age >= 14 && weekend === 'Saturday')) {
-    document.getElementById('para').innerHTML = ('Youre to old sorry!')
-  }
+  } else if ((age >= 14 && weekend === 'Sunday') || (age >= 14 && weekend === 'Saturday')) 
+    document.getElementById('para').innerHTML = ('Youre to old sorry!') 
 }
+
+// Those allow the website to know what to use which is what is inputed into the boxes, there also is if statements that tell you if tyhe number is larger or smaller than 14, than you cant go for free, and if the day is not saturday or sunday you also can not go into the Museum for free
