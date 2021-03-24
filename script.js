825e7c87239833ca7d22672343e2741e35aec1e2
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-24 15:01:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let age = 0 
+
+
+document.getElementById('button').addEventListener('click', button)
+
+function button () {
+  age = document.getElementById('input').value
+  weekend = document.getElementById('input2').value
+  if ((age < 14 && weekend === 'Sunday') || (age < 14 && weekend === 'Saturday')){
+    document.getElementById('para').innerHTML = (' You may go to the Museum for Free!')
+  } else if ((age >= 14 && weekend === 'Sunday') || (age > 14 && weekend === 'Saturday')){
+    document.getElementById('para').innerHTML = ('Youre to old sorry!')
+  }  
+}
