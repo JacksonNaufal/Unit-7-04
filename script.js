0e545f5446c0fa9596409e0ec4f5333907e03569
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-24 15:13:01 +0000

    Minor JS Errors

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let age = 0 
+let age = 0
 let weekend = 0
 document.getElementById('button').addEventListener('click', button)
 
 function button () {
   age = document.getElementById('input').value
   weekend = document.getElementById('input2').value
-  if ((age < 14 && weekend === 'Sunday') || (age < 14 && weekend === 'Saturday')){
+  if ((age < 14 && weekend === 'Sunday') || (age < 14 && weekend === 'Saturday')) {
     document.getElementById('para').innerHTML = (' You may go to the Museum for Free!')
   } else if ((age >= 14 && weekend === 'Sunday') || (age >= 14 && weekend === 'Saturday')){
     document.getElementById('para').innerHTML = ('Youre to old sorry!')
-  }  
+  }
 }
